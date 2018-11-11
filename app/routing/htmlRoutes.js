dd48723c90489b1307f2c9ced0deb4979d1f601d
Mike Gross <grossmi4@gmail.com>
2018-11-11 09:21:54 -0500

    home html rough done, server.js and htmlRoutes.js done

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,21 @@
+//dependencies
+
+const path = require("path");
+
+//routing
+
+module.exports = function(app) {
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"))
+  });
+
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+  });
+
+  //default path
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+  })
+};
---
+++ server.js
@@ -0,0 +1,16 @@
+//Adding dependencies
+const express = require("express");
+
+//Configuring Express
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json()); //using express JSON data handling
+
+//Setting routes from route files - passing express object in
+require("./app/routing/apiRoutes.js")(app);
+requite("./app/routing/htmlRoutes.js")(app);
+
+//Adding listener for server
+app.listen(PORT, function() {
+  console.log(`Server listening on port: ${PORT}`)
+});
